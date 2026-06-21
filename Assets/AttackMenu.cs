771268edd8b3d37e3bf4cfb1779cb5e4ab84f3ad
CeoOfGoogle1 <okdavidq@gmail.com>
2026-06-21 11:29:15 +0200

    some ass weapon display

---
+++ Assets/AttackMenu.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
+
+public class AttackMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    [SerializeField] private Transform buttonParent;
+    [SerializeField] private Button buttonPrefab;
+    public List<WeaponGroup> weaponGroups;
+
+    void Update()
+    {
+        panel.SetActive(true);
+
+        foreach (Transform child in buttonParent) Destroy(child.gameObject);
+
+        foreach (var group in weaponGroups)
+        {
+            Button btn = Instantiate(buttonPrefab, panel.transform, false);
+            btn.GetComponentInChildren<TMP_Text>().text = group.displayName;
+            btn.onClick.AddListener(() => SelectGroup(group));
+        }
+    }
+
+    public void ShowMenu()
+    {
+        panel.SetActive(true);
+
+        foreach (Transform child in buttonParent) Destroy(child.gameObject);
+
+        foreach (var group in weaponGroups)
+        {
+            Button btn = Instantiate(buttonPrefab, buttonParent, false);
+            btn.GetComponentInChildren<TMP_Text>().text = group.displayName;
+            btn.onClick.AddListener(() => SelectGroup(group));
+        }
+    }
+
+    private void SelectGroup(WeaponGroup group)
+    {
+        panel.SetActive(false);
+        // switch to that weapon group here
+    }
+}
